ff1e8c016031e181b7de97a15fa73492533ec0a4
iamshabell <91321698+iamshabell@users.noreply.github.com>
2024-10-12 02:32:57 +0300

    remove prints

--- src/message/transformer.rs
+++ src/message/transformer.rs
@@ -3,6 +3,5 @@ use serde_xml_rs::from_str;
 
 pub fn xml_to_json(xml: &str) -> Result<Value, serde_xml_rs::Error> {
     let json_data: Value = from_str(xml)?;
-    println!("Converted XML to JSON: {:?}", json_data);
     Ok(json_data)
 }
